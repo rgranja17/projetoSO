88caef9c613c77da581d96e35f721415d5da9ac5
Diogo José Fernandes Esteves <132235172+Diogoesteves11@users.noreply.github.com>
2024-05-02 18:04:47 +0100

    Delete include directory

--- include/engine.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENGINE_H
-#define ENGINE_H
-
-#include "../include/scheduler.h"
-
-Task __engine_execute_task(Task task_executing,int logFile_fd);
-Task __engine_execute_pipeline(Task task_executing,int logFile_fd);
-
-#endif
